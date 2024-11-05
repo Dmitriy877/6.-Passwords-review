293eab5b1aab06d7b65a25bbb4e32a388817b225
Dmitriy877 <dmitriybukin05@gmail.com>
2024-11-05 21:14:40 +0300

    #1

--- 6. Password review.py
+++ 6. Password review.py
@@ -1,1 +1,18 @@
-print('hello world')
+enter_password = input('Введите пароль: ')
+
+print('Длина пароля: ', len(enter_password))
+
+
+for symbol in range(len(enter_password)):
+    if enter_password.isdigit() == True:
+        print(enter_password[symbol], ' - Число')
+    else:
+        print(enter_password[symbol], ' - Буква')
+
+
+
+if len(enter_password) <= 12:
+    print('Короткий')
+else:
+    print('Длинный')
+
