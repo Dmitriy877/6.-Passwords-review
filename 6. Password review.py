e1366020a0cfbef14d26249f5f41409041edf5d5
Dmitriy877 <dmitriybukin05@gmail.com>
2024-11-06 22:29:17 +0300

    #2

--- 6. Password review.py
+++ 6. Password review.py
@@ -1,18 +1,34 @@
-enter_password = input('Введите пароль: ')
+# password = input('Введите пароль: ')
+#
+# print('Длина пароля: ', len(password))
+#
+#
+# for symbol in range(len(password)):
+#     if password[symbol].isdigit() == True:
+#         print(password[symbol], ' - Число')
+#     else:
+#         print(password[symbol], ' - Буква')
 
-print('Длина пароля: ', len(enter_password))
+
+def has_digit(password):
+    number_found = False
+    for symbol in range(len(password)):
+        if password[symbol].isdigit() == True:
+            number_found = True
+            return number_found
+    return number_found
 
 
-for symbol in range(len(enter_password)):
-    if enter_password.isdigit() == True:
-        print(enter_password[symbol], ' - Число')
-    else:
-        print(enter_password[symbol], ' - Буква')
+# if has_digit(password)== True:
+#     print("Цифры есть")
+# else:
+#     print("Цифр нет")
 
 
-
-if len(enter_password) <= 12:
+if len(password) <= 12:
     print('Короткий')
 else:
     print('Длинный')
 
+print(has_digit("rnfeinginr"))  # False
+print(has_digit("rnvnreiv83282"))  # True
